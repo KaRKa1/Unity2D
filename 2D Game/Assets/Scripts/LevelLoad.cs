6720538b210b109e74044b8a71b3a5bbf0f4f6da
KaRKa <seskusk@gmail.com>
2017-02-08 14:36:07 +0000

    Initializing project
    Made a simple main menu, added some music (Slider) and some resources.

---
+++ 2D Game/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour {
+
+    [SerializeField]
+    private int levelNo = 0;
+    
+	public void loadLevel ()
+    {
+        SceneManager.LoadScene(levelNo);
+    }
+
+}
